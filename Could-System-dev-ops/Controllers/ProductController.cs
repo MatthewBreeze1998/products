15f68d55aac889c9048f01254e2cd4dd67d1162c
MATTHEW BREEZE <T7077260@live.tees.ac.uk>
2019-12-17 23:09:21 +0000

    controller comments

--- Could-System-dev-ops/Controllers/ProductController.cs
+++ Could-System-dev-ops/Controllers/ProductController.cs
@@ -15,91 +15,96 @@ namespace Could_System_dev_ops.Controllers
     public class ProductsController : Controller
     {
         private ProductsRepo _ProductsRepo;
-        private ReSaleService _ReSaleList;
-        public ProductsController(ProductsRepo Products, ReSaleService reSale )
+        public ProductsController(ProductsRepo Products )
         {
             _ProductsRepo = Products;
-            _ReSaleList = reSale;
         }
-        [Route("CreatProduct/{product}")]
+        [Route("CreatProduct")]//Route
         [HttpPost]
-        public ActionResult<ProductsModel> CreateProdcuts(ProductsModel products)
+        public ActionResult<ProductsModel> CreateProdcuts(ProductsModel product)
         {
 
-            if(products == null)
+            if(product == null)// checks if Products is null
             {
+                return NotFound();// not found if null
+            }
+            if (product.ProductId <= 0)// checks valid id
+            {
+         
                 return NotFound();
             }
-            _ProductsRepo.CreateProduct(products);
-            return products;
+            return _ProductsRepo.CreateProduct(product); // calls xreate in interface and returns the new product
 
         }
-        [Route("EditProducts")]
+        [Route("EditProducts")]//Route
         [HttpPost]
         public ActionResult<ProductsModel> EditProduct(ProductsModel product)
         {
-            if(product == null)
+            if(product == null)// checks if Products is null
             {
+                return NotFound();// not found if null
+            }
+            if(product.ProductId <= 0)// checks valid id
+            {
+
                 return NotFound();
             }
-            _ProductsRepo.EditProducts(product);
-            return product;
+            
+            return _ProductsRepo.EditProducts(product); // calls edit fuction from interface
+            // retruns edited data
         }
 
 
-        [Route("GetProduct/{id}")]
+        [Route("GetProduct/{id}")]//Route
         [HttpGet]
         public ActionResult<ProductsModel> getProducts(int id)
         {
-            ProductsModel createProdcuts = _ProductsRepo.GetProduct(id);
-
-            if(createProdcuts == null)
+            if(id <= 0)// checks if id is valid
             {
-                return NotFound();
+                return NotFound();// not found if null
             }
 
-            return createProdcuts;
+            return _ProductsRepo.GetProduct(id); // gets prodcut by id returns product
         }
         
-        [Route("GetAllProducts")]
+        [Route("GetAllProducts")]//Route
         [HttpGet]
         public IEnumerable<ProductsModel> GetAllProducts()
         {
-            IEnumerable<ProductsModel> All = _ProductsRepo.GetAllProduct();
-            return All; 
+            return _ProductsRepo.GetAllProduct(); // call interface funcion and retuns all prodcuts as IEnumrable
         }
 
-        [Route("UpdateStock/{id,NewStock}")]
+        [Route("UpdateStock/{id,NewStock}")]//Route
         [HttpPost]
         public ActionResult<ProductsModel> UpdateStock(ProductsModel product, int NewStock)
         {     
-            if(product == null)
+            if(product == null)// checks if Products is null
             {
-                return NotFound();
+                return NotFound();// not found if null
             }
-            if(NewStock < 1)
+            if(NewStock < 1)// checks theres a new stock add
             {
-                return NotFound();
+                return NotFound();// not found if less than one
             }
-            product.StockLevel = product.StockLevel + NewStock;
-            return _ProductsRepo.EditProducts(product);
+            product.StockLevel = product.StockLevel + NewStock; // gets stocklevel gets new stock and adds them
+            return _ProductsRepo.EditProducts(product);// calls edit and updates the index with the new sock level
         }
 
       
         
-        [Route("SetReSale/{id}")]
+        [Route("SetReSale/{id}")]//Route
         [HttpPost]
         public ActionResult<ProductsModel> SetResale(ProductsReSaleUpdate Update)
         {
-            if(Update == null)
+            if(Update == null)// checks if Update is null
             {
-                return NotFound();
+                return NotFound();// not found if null
             }
 
-            ProductsModel products = Update.product;
-            ReSaleMetaData Resale = Update.ReSale;
-            products.Price = Resale.NewPrice;
-            return _ProductsRepo.EditProducts(products);
+            ProductsModel products = Update.product; // gives products a products model
+            ReSaleMetaData Resale = Update.ReSale;// gives resale resale metadata
+            products.Price = Resale.NewPrice; // changes old price to the resale price
+            return _ProductsRepo.EditProducts(products);// calls edit from interface and updates the index 
         }
 
 
