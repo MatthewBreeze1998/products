b005514c89f7e3732451c162c109598644764f2e
MATTHEW BREEZE <T7077260@live.tees.ac.uk>
2019-12-17 23:20:04 +0000

    comments

--- Could-System-dev-ops/Repo/FakeProductsRepo.cs
+++ Could-System-dev-ops/Repo/FakeProductsRepo.cs
@@ -20,29 +20,32 @@ namespace Could_System_dev_ops.Repo
                 new ProductsModel() {ProductId = 1,ProductName = "levi jeans", Description  =  "blue Jeans", Price = 123.12, StockLevel = 19},
                 new ProductsModel() {ProductId = 2,ProductName = "Black desk", Description  =  "Black desk", Price = 11.4 ,StockLevel = 3},
                 new ProductsModel() {ProductId = 3,ProductName = "Moniter", Description  =  "24' lg 1080p", Price = 341.41 ,StockLevel = 19}
-            };
+            };// test data 
         }
         public ProductsModel CreateProduct(ProductsModel products)
         {
             
-            _ProductsModelsList.Add(products);
-            return products;
+            _ProductsModelsList.Add(products);// products to test data
+            return products;// returrms the the new product
         }
 
         public ProductsModel GetProduct(int id)
         {
-            return _ProductsModelsList.FirstOrDefault(x => id == x.ProductId);
+            return _ProductsModelsList.FirstOrDefault(x => id == x.ProductId);// uses id to find product
+            // uses first or default to find the first product with that id
+            // retruns the product
         }
 
         public IEnumerable<ProductsModel> GetAllProduct()
         {
-            return _ProductsModelsList.AsEnumerable<ProductsModel>();
+            return _ProductsModelsList.AsEnumerable<ProductsModel>(); // retuns all prodcuts and returns them as an IEnumerable
         }
 
         public ProductsModel EditProducts(ProductsModel products)
         {
-            _ProductsModelsList[_ProductsModelsList.IndexOf(_ProductsModelsList.FirstOrDefault(x => x.ProductId == products.ProductId))] = products;
-            return products;
+            
+            return _ProductsModelsList[_ProductsModelsList.IndexOf(_ProductsModelsList.FirstOrDefault(x => x.ProductId == products.ProductId))] = products;
+            // finds the product with the id then replaces the index with the new product model thats passed through
         }
     }
 }
