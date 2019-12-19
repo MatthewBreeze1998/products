a607b3bb6f262677657277d131cb56a3e5ab54c8
MATTHEW BREEZE <T7077260@live.tees.ac.uk>
2019-12-19 22:33:02 +0000

    setting up test/ name chnages

--- ProductsControllerTest/UnitTest1.cs
+++ ProductsControllerTest/UnitTest1.cs
@@ -1,6 +1,7 @@
 using Could_System_dev_ops.Controllers;
 using Could_System_dev_ops.Models;
 using Could_System_dev_ops.Repo;
+using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -35,10 +36,71 @@ namespace ProductsControllerTest
 
         }
 
-        [Test]
-        public void Test1()
+        public void CreateProduct_validUser_ShouldObject()
         {
-            Assert.Pass();
+
+
+            Assert.IsNotNull(_ProductsRepo);
+            Assert.IsNotNull(_ProductsController);
+            ProductsModel product = new ProductsModel() { ProductId = 4, ProductName = "levi jeans", Description = "blue Jeans", Price = 123.12, StockLevel = 19 };
+            Assert.IsNotNull(user);
+
+            int currentMaxId = _ProductsController.getProducts().Value.Max();
+            Assert.GreaterOrEqual(currentMaxId, 1);
+
+            ActionResult<ProductsModel> result = _ProductsController.CreateProdcuts(product);
+            Assert.IsNotNull(result);
+
+            ActionResult usersResult = result.Result;
+            Assert.AreEqual(usersResult.GetType(), typeof(CreatedAtActionResult));
+
+            CreatedAtActionResult createdUserResult = (CreatedAtActionResult)usersResult;
+            Assert.IsNotNull(createdUserResult);
+            Assert.AreEqual(createdUserResult.Value.GetType(), typeof(ProductsModel));
+
+            ProductsModel UserValue = (ProductsModel)createdUserResult.Value;
+            Assert.IsNotNull(UserValue);
+
+            Assert.AreEqual(currentMaxId + 1, UserValue.UserId);
+            Assert.AreEqual(user.FirstName, UserValue.FirstName);
+            Assert.AreEqual(user.LastName, UserValue.LastName);
+            Assert.AreEqual(user.Email, UserValue.Email);
+            Assert.AreEqual(user.isActive, UserValue.isActive);
+            Assert.AreEqual(user.PurchaseAbility, user.PurchaseAbility);
+        }
+
+        [Test]
+        public void CreateProduct_InvalidUser_ShouldObject()
+        {
+            _userRepo = new FakeUserRepo(null);
+
+            Assert.IsNotNull(_userRepo);
+            Assert.IsNotNull(_userController);
+            UsersModel user = new UsersModel() { UserId = 0, FirstName = "", LastName = "", Email = "", isActive = false, PurchaseAbility = false };
+            Assert.IsNotNull(user);
+
+            int currentMaxId = _userController.GetAllUsers().;
+            Assert.GreaterOrEqual(currentMaxId, 1);
+
+            ActionResult<UsersModel> result = _userController.CreateUser(user);
+            Assert.IsNotNull(result);
+
+            ActionResult usersResult = result.Result;
+            Assert.AreEqual(usersResult.GetType(), typeof(CreatedAtActionResult));
+
+            CreatedAtActionResult createdUserResult = (CreatedAtActionResult)usersResult;
+            Assert.IsNotNull(createdUserResult);
+            Assert.AreEqual(createdUserResult.Value.GetType(), typeof(UsersModel));
+
+            UsersModel UserValue = (UsersModel)createdUserResult.Value;
+            Assert.IsNotNull(UserValue);
+
+            Assert.AreEqual(currentMaxId + 1, UserValue.UserId);
+            Assert.AreEqual(user.FirstName, UserValue.FirstName);
+            Assert.AreEqual(user.LastName, UserValue.LastName);
+            Assert.AreEqual(user.Email, UserValue.Email);
+            Assert.AreEqual(user.isActive, UserValue.isActive);
+            Assert.AreEqual(user.PurchaseAbility, user.PurchaseAbility);
         }
     }
 }
