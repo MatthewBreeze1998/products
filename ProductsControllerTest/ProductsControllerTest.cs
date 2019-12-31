711e54d166eb86a37b4ac48e8e504a12fb4f3c70
MATTHEW BREEZE <T7077260@live.tees.ac.uk>
2019-12-31 21:33:49 +0000

    formatting and comments

--- ProductsControllerTest/ProductsControllerTest.cs
+++ ProductsControllerTest/ProductsControllerTest.cs
@@ -39,88 +39,85 @@ namespace ProductsControllerTest
         [Test]
         public void CreateProduct_validProduct_ShouldObject()
         {
-            Assert.IsNotNull(_ProductsRepo);
-            Assert.IsNotNull(_ProductsController);
-            ProductsModel product = new ProductsModel() { ProductId = 4, ProductName = "levi jeans", Description = "blue Jeans", Price = 123.12, StockLevel = 19 };
+            Assert.IsNotNull(_ProductsRepo);// not null repo
+            Assert.IsNotNull(_ProductsController);// not null controller
+            ProductsModel product = new ProductsModel() { ProductId = 4, ProductName = "levi jeans", Description = "blue Jeans", Price = 123.12, StockLevel = 19 };// valid new model 
             Assert.IsNotNull(product);
 
-            int currentMaxId = _ProductsController.GetAllProducts().Max(x => x.ProductId);
-            Assert.GreaterOrEqual(currentMaxId, 1);
+            int currentMaxId = _ProductsController.GetAllProducts().Max(x => x.ProductId);// gets max product id 
+            Assert.GreaterOrEqual(currentMaxId, 1);// adds 1 to max
 
-            ActionResult<ProductsModel> result = _ProductsController.CreateProdcut(product);
-            Assert.IsNotNull(result);
+            ActionResult<ProductsModel> result = _ProductsController.CreateProdcut(product);// result is return of create controller function
+            Assert.IsNotNull(result);// result is nit null
 
-            ActionResult ProductResult = result.Result;
-            Assert.AreEqual(ProductResult.GetType(), typeof(CreatedAtActionResult));
+            ActionResult ProductResult = result.Result;// ProductResult is result.Result
+            Assert.AreEqual(ProductResult.GetType(), typeof(CreatedAtActionResult));//  ProductResult is of type create at action
 
-            CreatedAtActionResult createdProductResult = (CreatedAtActionResult)ProductResult;
-            Assert.IsNotNull(createdProductResult);
-            Assert.AreEqual(createdProductResult.Value.GetType(), typeof(ProductsModel));
+            CreatedAtActionResult createdProductResult = (CreatedAtActionResult)ProductResult;// new create at action ProductResult 
+            Assert.IsNotNull(createdProductResult);// createdProductResult is not null
+            Assert.AreEqual(createdProductResult.Value.GetType(), typeof(ProductsModel));// createdProductResult is of tpye product model
 
-            ProductsModel ProductValue = (ProductsModel)createdProductResult.Value;
-            Assert.IsNotNull(ProductValue);
+            ProductsModel ProductValue = (ProductsModel)createdProductResult.Value;// ProductValue is createdProductResult.Value
+            Assert.IsNotNull(ProductValue);// is not null
 
-            Assert.AreEqual(currentMaxId + 1, ProductValue.ProductId);
-            Assert.AreEqual(product.ProductName, ProductValue.ProductName);
-            Assert.AreEqual(product.Description, ProductValue.Description);
-            Assert.AreEqual(product.Price, ProductValue.Price);
-            Assert.AreEqual(product.StockLevel, ProductValue.StockLevel);
+            Assert.AreEqual(currentMaxId + 1, ProductValue.ProductId);// are equal 
+            Assert.AreEqual(product.ProductName, ProductValue.ProductName);// are equal 
+            Assert.AreEqual(product.Description, ProductValue.Description);// are equal 
+            Assert.AreEqual(product.Price, ProductValue.Price);// are equal 
+            Assert.AreEqual(product.StockLevel, ProductValue.StockLevel);// are equal 
         }
 
         [Test]
         public void CreateProduct_InvalidProduct_ShouldObject()
         {
-           
-      
-            Assert.IsNotNull(_ProductsRepo);
-            Assert.IsNotNull(_ProductsController);
-            ProductsModel product = null;
-            Assert.IsNull(product);
 
-            ActionResult<ProductsModel> result = _ProductsController.CreateProdcut(product);
-            Assert.IsNotNull(result);
 
-            ActionResult ProductResult = result.Result;
-            Assert.AreEqual(ProductResult.GetType(), typeof(BadRequestResult));
+            Assert.IsNotNull(_ProductsRepo);// not null repo
+            Assert.IsNotNull(_ProductsController);// not null controller
+            ProductsModel product = null;// null product model
+            Assert.IsNull(product);// is null
+
+            ActionResult<ProductsModel> result = _ProductsController.CreateProdcut(product);// calls create with null model 
+            Assert.IsNotNull(result);// result is nit null
+
+            ActionResult ProductResult = result.Result;// ProductResult is result. result
+            Assert.AreEqual(ProductResult.GetType(), typeof(BadRequestResult));// ProductResult is of bad request
         }
      
         
         [Test]
         public void DeleteProduct_valid_ShouldObject()
         {
-          
-                Assert.IsNotNull(_ProductsRepo);
-                Assert.IsNotNull(_ProductsController);
-                ProductsModel DeleteProduct = new ProductsModel() { ProductId = 2, ProductName = "Black desk", Description = "Black desk", Price = 11.4, StockLevel = 3 };
-                Assert.IsNotNull(DeleteProduct);
 
-                ActionResult<ProductsModel> product = _ProductsController.DeleteProduct(DeleteProduct); 
-                Assert.IsNotNull(product);
-                Assert.IsNotNull(product.Value);
+                Assert.IsNotNull(_ProductsRepo);// not null repo
+                Assert.IsNotNull(_ProductsController);// not null controller;
+                ProductsModel DeleteProduct = new ProductsModel() { ProductId = 2, ProductName = "Black desk", Description = "Black desk", Price = 11.4, StockLevel = 3 };// valid product model
+                Assert.IsNotNull(DeleteProduct);// DeleteProduct is not null
 
-                ActionResult<ProductsModel> result = _ProductsController.getProduct(product.Value.ProductId);
-                Assert.IsNotNull(result);
+                ActionResult<ProductsModel> product = _ProductsController.DeleteProduct(DeleteProduct); // product is the return of DeleteProduct
+                Assert.IsNotNull(product);// product is not null 
+                Assert.IsNotNull(product.Value);// product. value is not null
 
-                ActionResult StaffResult = result.Result;
-                Assert.AreEqual(StaffResult.GetType(), typeof(NotFoundResult));
+                ActionResult<ProductsModel> result = _ProductsController.getProduct(product.Value.ProductId);// result is result of get product
+                Assert.IsNotNull(result);// is nit null
 
-            
-
+                ActionResult StaffResult = result.Result;//StaffResult is result.result
+                Assert.AreEqual(StaffResult.GetType(), typeof(NotFoundResult));// StaffResult is of type bad request
         }
 
         [Test]
         public void DeleteProduct_invalid_ShouldObject()
         {
-            Assert.IsNotNull(_ProductsRepo);
-            Assert.IsNotNull(_ProductsController);
-            ProductsModel product = null;
-            Assert.IsNull(product);
+            Assert.IsNotNull(_ProductsRepo);// not null repo
+            Assert.IsNotNull(_ProductsController);// not null controller;
+            ProductsModel product = null;// prodcut is null product model
+            Assert.IsNull(product);// product is null
 
-            ActionResult<ProductsModel> result = _ProductsController.DeleteProduct(product); 
-            Assert.IsNotNull(result);
+            ActionResult<ProductsModel> result = _ProductsController.DeleteProduct(product); // result is return if DeleteProduct
+            Assert.IsNotNull(result);// result is not null
 
-            ActionResult ProductResult = result.Result;
-            Assert.AreEqual(ProductResult.GetType(), typeof(BadRequestResult));
+            ActionResult ProductResult = result.Result;// ProductResult is result.Result
+            Assert.AreEqual(ProductResult.GetType(), typeof(BadRequestResult));//ProductResult is type of bad request cause product is null
         }
     }
 }
