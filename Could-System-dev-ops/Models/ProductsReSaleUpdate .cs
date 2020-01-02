7e605407e754b3803d30bc496529383070b9ef5f
T7077260 <T7077260@TU39291>
2020-01-02 15:55:24 +0000

    could to cloud

--- Could-System-dev-ops/Models/ProductsReSaleUpdate .cs
+++ Could-System-dev-ops/Models/ProductsReSaleUpdate .cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Could_System_dev_ops.Models
+namespace Cloud_System_dev_ops_System_dev_ops.Models
 {
     public class ProductsReSaleUpdate
     {
